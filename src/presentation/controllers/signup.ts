5f43f9b11bebecf687d959b19299c8f4a31603aa
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-21 23:58:36 -0300

    feat: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,9 +2,18 @@ export default class SignUpController {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name'),
-    };
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      };
+    }
+    return {};
   }
 }
